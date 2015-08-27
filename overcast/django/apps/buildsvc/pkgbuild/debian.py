c4a0b9a4d1fa62859f68a603eb8e55e7ab70aab2
Soren Hansen <Soren.Hansen@ril.com>
2015-08-27 17:51:24 +0200

    Use name from control file in changelog

--- overcast/django/apps/buildsvc/pkgbuild/debian.py
+++ overcast/django/apps/buildsvc/pkgbuild/debian.py
@@ -1,4 +1,5 @@
 import os.path
+import debian.deb822
 
 from ..utils import run_cmd
 from ..pkgbuild import PackageBuilder, PackageBuilderRegistry
@@ -13,6 +14,11 @@ class DebianBuilder(PackageBuilder):
     def native_version(self):
         cmd = ['dpkg-parsechangelog', '--show-field', 'Version']
         return run_cmd(cmd, cwd=self.builddir).strip()
+
+    @property
+    def package_name(self):
+        ctrl = debian.deb822.Deb822(open(os.path.join(self.builddir, 'debian/control'), 'r'))
+        return ctrl['Source']
 
     def populate_debian_dir(self):
         pass
