6acce0ab902ed97248f338ad966dac0284393bee
Soren Hansen <Soren.Hansen@ril.com>
2015-09-14 22:13:22 +0200

    Make repositories include a link to the list of series

--- overcast/django/apps/buildsvc/serializers.py
+++ overcast/django/apps/buildsvc/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework_nested import relations
 
 from . import models
 
@@ -16,13 +17,6 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'name')
 
 
-class RepositorySerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    class Meta:
-        model = models.Repository
-        fields = ('url', 'user', 'name', 'key_id') #, 'series_set')
-
-
 class PackageSourceSerializer(serializers.HyperlinkedModelSerializer):
     git_repository = serializers.ReadOnlyField(source='github_repository.url')
     git_branch = serializers.ReadOnlyField(source='branch')
@@ -36,3 +30,12 @@ class SeriesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Series
         fields = ('url', 'name', 'repository', 'binary_source_list', 'source_source_list')
+
+
+class RepositorySerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    series = relations.HyperlinkedRouterField(view_name='series-list', lookup_url_kwarg='repository_pk', lookup_field='pk', read_only=True)
+
+    class Meta:
+        model = models.Repository
+        fields = ('url', 'user', 'name', 'key_id', 'series_set')
