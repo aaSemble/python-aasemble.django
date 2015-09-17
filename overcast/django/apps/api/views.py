fc5cf4ebbed17c20c39acb06f8a2047643624935
Soren Hansen <Soren.Hansen@ril.com>
2015-09-17 16:02:34 +0200

    Make Github callback configurable

--- overcast/django/apps/api/views.py
+++ overcast/django/apps/api/views.py
@@ -1,8 +1,10 @@
+from django.conf import settings
+
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
 
 class GithubLogin(SocialLoginView):
-    callback_url = 'http://dev.overcastcloud.com/static/index.html'
+    callback_url = settings.GITHUB_AUTH_CALLBACK
     adapter_class = GitHubOAuth2Adapter
     client_class =  OAuth2Client
