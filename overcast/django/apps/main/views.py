3f3e1d7065561fe52f70b792c21e2ddac03ad1a3
Soren Hansen <Soren.Hansen@ril.com>
2015-09-22 13:36:19 +0200

    Move templates into subdir to avoid clashing with allauth

--- overcast/django/apps/main/views.py
+++ overcast/django/apps/main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     if request.user.is_authenticated():
-        return render(request, 'dashboard.html', {})
+        return render(request, 'overcast/dashboard.html', {})
     else:
-        return render(request, 'front.html', {})
+        return render(request, 'overcast/front.html', {})
 
 
