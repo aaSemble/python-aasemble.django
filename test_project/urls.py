b8c85c36b4bab3624242b262b38a8c9986e006c3
Soren Hansen <Soren.Hansen@ril.com>
2015-09-28 11:33:32 +0200

    Add allauth urls

--- test_project/urls.py
+++ test_project/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^api/', include('overcast.django.apps.api.urls')),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^profile/$', 'overcast.django.apps.main.views.profile', name='profile'),
     url(r'^$', 'overcast.django.apps.main.views.index', name='index'),
     url(r'^buildsvc/', include('overcast.django.apps.buildsvc.urls', namespace='buildsvc')),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
