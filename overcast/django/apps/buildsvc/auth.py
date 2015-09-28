1a6d448aa4b744a51df01f1572fe8a9db26b1d03
Soren Hansen <Soren.Hansen@ril.com>
2015-09-28 12:42:58 +0200

    How on Earth did this pass testing?

--- overcast/django/apps/buildsvc/auth.py
+++ overcast/django/apps/buildsvc/auth.py
@@ -6,17 +6,17 @@ class BuildSvcAuthzBackend(object):
         if not user_obj.is_active:
             return False
 
-        app, perm_ = perm.split('.'):
-            if app == 'buildsvc':
-                action, model = perm_.split('_')
-                if (model in ('packagesource', 'externaldependency') and
-                    action in ('add', 'delete', 'change')):
-                    # This is pretty nuts. If we don't return True here,
-                    # django-rest-framework aborts immediately. So, we say
-                    # "sure!" and hope it asks for permission for the specific
-                    # object.
-                    if obj is None:
-                        return True
+        app, perm_ = perm.split('.')
+        if app == 'buildsvc':
+            action, model = perm_.split('_')
+            if (model in ('packagesource', 'externaldependency') and
+                action in ('add', 'delete', 'change')):
+                # This is pretty nuts. If we don't return True here,
+                # django-rest-framework aborts immediately. So, we say
+                # "sure!" and hope it asks for permission for the specific
+                # object.
+                if obj is None:
+                    return True
 
-                if hasattr(obj, 'user_can_modify'):
-                    return obj.user_can_modify(user_obj)
+            if hasattr(obj, 'user_can_modify'):
+                return obj.user_can_modify(user_obj)
