980ddf46f25d32d83f0fc4ac532354362340c51f
Soren Hansen <Soren.Hansen@ril.com>
2015-10-01 13:57:39 +0200

    Also add code for mirrorsvc
    We should move this code higher up the dir structure

--- overcast/django/apps/buildsvc/auth.py
+++ overcast/django/apps/buildsvc/auth.py
@@ -7,16 +7,19 @@ class BuildSvcAuthzBackend(object):
             return False
 
         app, perm_ = perm.split('.')
-        if app == 'buildsvc':
-            action, model = perm_.split('_')
-            if (model in ('packagesource', 'externaldependency') and
-                action in ('add', 'delete', 'change')):
-                # This is pretty nuts. If we don't return True here,
-                # django-rest-framework aborts immediately. So, we say
-                # "sure!" and hope it asks for permission for the specific
-                # object.
-                if obj is None:
-                    return True
+        action, model = perm_.split('_')
+
+        if action in ('add', 'delete', 'change'):
+            return False
+
+        if (((app == 'buildsvc') and (model in ('packagesource', 'externaldependency'))) or
+            ((app == 'mirrorsvc') and (model in ('mirror', 'mirrorset', 'snapshot')))):
+            # This is pretty nuts. If we don't return True here,
+            # django-rest-framework aborts immediately. So, we say
+            # "sure!" and hope it asks for permission for the specific
+            # object.
+            if obj is None:
+                return True
 
             if hasattr(obj, 'user_can_modify'):
                 return obj.user_can_modify(user_obj)
