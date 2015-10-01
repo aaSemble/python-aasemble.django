a1e33ef1320b62e929ae06a63a2c598a23f69d35
Soren Hansen <Soren.Hansen@ril.com>
2015-10-01 09:17:04 +0200

    Refresh settings.py
    I had git ignore settings.py, so I forgot to commit all these changes as I went
    along. It no longer has any credentials in it, so now it should be safe enough
    to share it.

--- test_project/settings.py
+++ test_project/settings.py
@@ -25,18 +25,27 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.humanize',
-    'social.apps.django_app.default',
     'overcast.django.apps.main',
+    'overcast.django.apps.api',
     'overcast.django.apps.buildsvc',
     'overcast.django.apps.mirrorsvc',
-    'kombu.transport.django',
+    'bootstrap3',
     'djcelery',
     'rest_framework',
-    'bootstrap3',
+    'rest_framework.authtoken',
+    'kombu.transport.django',
+    'corsheaders',
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -66,7 +75,7 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'social.backends.github.GithubOAuth2',
+    'allauth.account.auth_backends.AuthenticationBackend',
     'django.contrib.auth.backends.ModelBackend',
     'overcast.django.apps.buildsvc.auth.BuildSvcAuthzBackend',
 )
@@ -91,16 +100,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
-SOCIAL_AUTH_GITHUB_KEY = '6b506b92dd00942dcfc8'
-SOCIAL_AUTH_GITHUB_SECRET = '28f3e70926faa462c50ec5b5c238d5c8af547d72'
-SOCIAL_AUTH_GITHUB_SCOPE = ['read:org']
-
 BUILDSVC_REPOS_BASE_DIR = os.path.join(BASE_DIR, 'data', 'repos')
 BUILDSVC_REPOS_BASE_PUBLIC_DIR = os.path.join(BASE_DIR, 'data', 'public_repos')
 BUILDSVC_REPOS_BASE_URL = 'http://127.0.0.1:8000/apt/'
 BUILDSVC_DEFAULT_SERIES_NAME = 'overcast'
 BUILDSVC_DEBEMAIL = 'pkgbuild@overcastcloud.com'
 BUILDSVC_DEBFULLNAME = 'Overcast Package Building Service'
+MIRRORSVC_BASE_PATH = os.path.join(BASE_DIR, 'mirrors')
 
 LOGIN_URL = '/login/github/'
 LOGIN_REDIRECT_URL = '/'
@@ -125,8 +131,45 @@ CELERYBEAT_SCHEDULE = {
 }
 
 CELERY_TIMEZONE = TIME_ZONE
-
+GITHUB_AUTH_CALLBACK = 'http://localhost:8000/'
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'URL_FIELD_NAME': 'self',
 }
+
+CORS_ORIGIN_WHITELIST = ()
+CORS_ALLOW_CREDENTIALS = True
+
+# Request access to user's org memberships
+SOCIALACCOUNT_PROVIDERS = {'github': {'SCOPE': ['read:org']}}
+
+# Don't send account verification e-mails
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
+# Use https when generating links for e-mails
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
+
+# Use e-mail address as the "username". Not a username.
+# Not a username or e-mail address. Just an e-mail address.
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+# There has to be an e-mail address. ACCOUNT_AUTHENTICATION_METHOD='email' requires it.
+ACCOUNT_EMAIL_REQUIRED = True
+
+# Usernames suck. People forget them, the good ones are always taken, etc. E-mail.
+ACCOUNT_USERNAME_REQUIRED = False
+
+# Users can sign up via Github
+SOCIALACCOUNT_AUTO_SIGNUP=True
+
+# Must e-mail addresses be unique? Hells yeah!
+ACCOUNT_UNIQUE_EMAIL=True
+
+
+REST_SESSION_LOGIN = False
+
+DEFAULT_FROM_EMAIL = 'noreply@overcasatcloud.com'
