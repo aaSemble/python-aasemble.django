231a95d6d7cbcd14d09d53ce00d2a31d331ec974
Soren Hansen <Soren.Hansen@ril.com>
2015-10-08 11:07:58 +0200

    Remove reference to old social auth code

--- test_project/urls.py
+++ test_project/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     url(r'^$', 'overcast.django.apps.main.views.index', name='index'),
     url(r'^buildsvc/', include('overcast.django.apps.buildsvc.urls', namespace='buildsvc')),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
-    url('', include('social.apps.django_app.urls', namespace='social'))
 ]
