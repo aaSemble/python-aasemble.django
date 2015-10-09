c509f6c27d59657fad7715e2a7e3b3de4ac79908
Soren Hansen <Soren.Hansen@ril.com>
2015-10-09 14:56:27 +0200

    Add API unit tests for sources

--- overcast/django/apps/api/tests.py
+++ overcast/django/apps/api/tests.py
@@ -10,41 +10,40 @@ def authenticate(client, username=None, token=None):
 class APIv1Tests(APITestCase):
     fixtures = ['data2.json']
 
+class APIv1RepositoryTests(APIv1Tests):
+    list_url = '/api/v1/repositories/'
+
     def test_create_repository_empty_fails_400(self):
-        url = '/api/v1/repositories/'
         data = {}
         authenticate(self.client, 'testuser')
 
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.list_url, data, format='json')
         self.assertEquals(response.status_code, 400)
         self.assertEquals(response.data, {'name': ['This field is required.']})
 
 
     def test_create_repository_no_auth_fails_401(self):
-        url = '/api/v1/repositories/'
         data = {}
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.list_url, data, format='json')
         self.assertEquals(response.status_code, 401)
 
 
     def test_create_repository_incorrect_auth_fails_401(self):
-        url = '/api/v1/repositories/'
         data = {}
         authenticate(self.client, token='invalidtoken')
 
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.list_url, data, format='json')
 
         self.assertEquals(response.status_code, 401)
 
     def test_create_repository(self):
-        url = '/api/v1/repositories/'
         data = {'name': 'testrepo'}
         authenticate(self.client, 'testuser')
 
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.list_url, data, format='json')
 
         self.assertEquals(response.status_code, 201)
-        self.assertTrue(response.data['self'].startswith('http://testserver/api/v1/repositories/'))
+        self.assertTrue(response.data['self'].startswith('http://testserver' + self.list_url))
         expected_result = {'external_dependencies': response.data['self'] + 'external_dependencies/',
                            'name': 'testrepo',
                            'binary_source_list': 'deb http://127.0.0.1:8000/apt/testuser/testrepo overcast main',
@@ -57,3 +56,62 @@ class APIv1Tests(APITestCase):
         self.assertEquals(response.data, expected_result)
         response = self.client.get(response.data['self'])
         self.assertEquals(response.data, expected_result)
+
+
+class APIv1SourceTests(APIv1Tests):
+    fixtures = ['data2.json', 'repository.json']
+
+    list_url = '/api/v1/sources/'
+
+    def test_create_source_empty_fails_400(self):
+        data = {}
+        authenticate(self.client, 'testuser')
+
+        response = self.client.post(self.list_url, data, format='json')
+        self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.data, {'git_repository': ['This field is required.'],
+                                          'git_branch': ['This field is required.'],
+                                          'repository': ['This field is required.']})
+
+
+    def test_create_invalied_url_fails_400(self):
+        data = {'git_repository': 'not a valid url'}
+        authenticate(self.client, 'testuser')
+
+        response = self.client.post(self.list_url, data, format='json')
+        self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.data, {'git_repository': ['Enter a valid URL.'],
+                                          'git_branch': ['This field is required.'],
+                                          'repository': ['This field is required.']})
+
+
+    def test_create_source_no_auth_fails_401(self):
+        data = {}
+        response = self.client.post(self.list_url, data, format='json')
+        self.assertEquals(response.status_code, 401)
+
+
+    def test_create_source_incorrect_auth_fails_401(self):
+        data = {}
+        authenticate(self.client, token='invalidtoken')
+
+        response = self.client.post(self.list_url, data, format='json')
+
+        self.assertEquals(response.status_code, 401)
+
+    def test_create_source(self):
+        data = {'git_repository': 'https://github.com/sorenh/buildsvctest',
+                'git_branch': 'master',
+                'repository': 'http://testserver/api/v1/repositories/1/'}
+        authenticate(self.client, 'testuser')
+
+        response = self.client.post(self.list_url, data, format='json')
+
+        self.assertEquals(response.status_code, 201)
+        self.assertTrue(response.data['self'].startswith('http://testserver' + self.list_url))
+        data['self'] = response.data['self']
+        data['builds'] = data['self'] + 'builds/'
+        self.assertEquals(response.data, data)
+
+        response = self.client.get(data['self'])
+        self.assertEquals(response.data, data)
