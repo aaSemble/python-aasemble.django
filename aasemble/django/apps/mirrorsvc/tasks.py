0c3f8a952af80fd9e1d65e572e5d26b4f6797acf
Soren Hansen <Soren.Hansen@ril.com>
2015-10-30 11:32:49 +0100

    Move snapshot code to a task

--- aasemble/django/apps/mirrorsvc/tasks.py
+++ aasemble/django/apps/mirrorsvc/tasks.py
@@ -5,3 +5,10 @@ def refresh_mirror(mirror_id):
     from .models import Mirror
     ps = Mirror.objects.get(id=mirror_id)
     ps.update_mirror()
+
+
+@shared_task(ignore_result=True)
+def perform_snapshot(snapshot_id):
+    from .models import Snapshot
+    s = Snapshot.objects.get(id=snapshot_id)
+    s.perform_snapshot()
