53666cb87722faed79941331429355b7bd80a841
Soren Hansen <Soren.Hansen@ril.com>
2015-11-10 14:52:49 +0100

    Add some tests for run_cmd

--- aasemble/django/tests.py
+++ aasemble/django/tests.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 from django.test import TestCase, override_settings
 from django.conf import settings
 from django.contrib.auth import (
@@ -6,6 +9,14 @@ from django.contrib.auth import (
 from django.contrib.sessions.backends.db import SessionStore
 from django.contrib.auth.models import User
 
+from aasemble.django.utils import run_cmd
+from aasemble.django.exceptions import CommandFailed
+
+stdout_stderr_script = '''#!/bin/sh
+
+echo stdout
+echo stderr >&2
+'''
 
 @override_settings(BUILDSVC_REPODRIVER='aasemble.django.apps.buildsvc.models.FakeDriver')
 class AasembleTestCase(TestCase):
@@ -31,3 +42,58 @@ def create_session_cookie(username, password):
         'path': '/',
     }
     return cookie
+
+class UtilsTestCase(AasembleTestCase):
+    def test_run_cmd_dead_simple(self):
+        # Should simply return successfully
+        stdout = run_cmd(['true'])
+        self.assertEquals(stdout, '')
+
+    def test_run_cmd_fail_raises_exception(self):
+        self.assertRaises(CommandFailed, run_cmd, ['false'])
+
+    def test_run_cmd_override_env(self):
+        os.environ['TESTVAR'] = 'foo'
+        stdout = run_cmd(['env'])
+        self.assertIn('TESTVAR=foo', stdout)
+
+        stdout = run_cmd(['env'], override_env={'TESTVAR': 'bar'})
+        self.assertIn('TESTVAR=bar', stdout)
+
+        stdout = run_cmd(['env'], override_env={'TESTVAR': None})
+        self.assertNotIn('TESTVAR=', stdout)
+
+    def test_run_cmd_other_cwd(self):
+        self.assertEquals(run_cmd(['pwd'], cwd='/').strip(), '/')
+
+    def _prepare_stdout_stderr_script(self):
+        _fd, tmpfile = tempfile.mkstemp()
+        try:
+            os.close(_fd)
+            with open(tmpfile, 'w') as fp:
+                fp.write(stdout_stderr_script)
+            os.chmod(tmpfile, 0755)
+            return tmpfile
+        except:
+            os.unlink(tmpfile)
+            raise
+
+    def test_run_cmd_stdout_includes_stderr(self):
+        tmpfile = self._prepare_stdout_stderr_script()
+        try:
+            with open(tmpfile, 'w') as fp:
+                fp.write(stdout_stderr_script)
+            os.chmod(tmpfile, 0755)
+            self.assertIn('stderr', run_cmd([tmpfile]))
+        finally:
+            os.unlink(tmpfile)
+
+    def test_run_cmd_stdout_can_discard_stderr(self):
+        tmpfile = self._prepare_stdout_stderr_script()
+        try:
+            with open(tmpfile, 'w') as fp:
+                fp.write(stdout_stderr_script)
+            os.chmod(tmpfile, 0755)
+            self.assertNotIn('stderr', run_cmd([tmpfile], discard_stderr=True))
+        finally:
+            os.unlink(tmpfile)
