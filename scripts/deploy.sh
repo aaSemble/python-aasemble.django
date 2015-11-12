45b8fa95a41083ce534bae067329ed5373a7a792
Soren Hansen <Soren.Hansen@ril.com>
2015-11-12 13:43:19 +0100

    Only run deploy in Django1.8 + py2.7 env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-ls
-pwd
+if [ "$DJANGO_VERSION" != "1.8" ] || [ "$TRAVIS_PYTHON_VERSION" != "2.7" ]
+then
+    # Don't want to run a deploy for each cell in the test matrix
+    exit 0
+fi
+
 openssl aes-256-cbc -K $encrypted_8f2ce7add2e8_key -iv $encrypted_8f2ce7add2e8_iv -in deploy.key.enc -out deploy.key -d || echo it failed
-ls
 chmod 600 deploy.key
-env | grep -v encrypted
-: ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@overcastcloud.com 'cd www; ./deploy.sh ; cd ../dev/current ; ./deploy.sh'
+ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@overcastcloud.com 'cd www; ./deploy.sh ; cd ../dev/current ; ./deploy.sh'
