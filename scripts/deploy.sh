bad4fb1e5e9af4382a1b614e389570e6f9acc553
Soren Hansen <Soren.Hansen@ril.com>
2015-11-12 13:36:03 +0100

    Dump environment
    I am looking for something to filter deploy execution on.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,5 @@ pwd
 openssl aes-256-cbc -K $encrypted_8f2ce7add2e8_key -iv $encrypted_8f2ce7add2e8_iv -in deploy.key.enc -out deploy.key -d || echo it failed
 ls
 chmod 600 deploy.key
-ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@overcastcloud.com 'cd www; ./deploy.sh ; cd ../dev/current ; ./deploy.sh'
+env | grep -v encrypted
+: ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@overcastcloud.com 'cd www; ./deploy.sh ; cd ../dev/current ; ./deploy.sh'
