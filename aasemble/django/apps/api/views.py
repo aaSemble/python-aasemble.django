ba66269a45ee9e21c9c62a8284c83464099572d4
Soren Hansen <Soren.Hansen@ril.com>
2015-11-12 18:13:20 +0100

    Add a handler for Github webhook events

---
+++ aasemble/django/apps/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+class GithubHookView(CreateAPIView):
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        from .tasks import github_push_event
+
+        event_type = request.META['HTTP_X_GITHUB_EVENT']
+
+        if event_type != 'push':
+            return Response({'thanks': 'but no thanks'})
+
+        github_push_event.delay(request.data['repository']['url'])
+
+        return Response({'ok': 'thanks'})
