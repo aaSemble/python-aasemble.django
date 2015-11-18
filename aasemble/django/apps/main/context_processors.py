5e79e3a1ee7655637daa808a3b376118924f2ee3
Mayank Kapoor <mayankkapoor@gmail.com>
2015-11-18 12:10:44 +0530

    Now using getattr

--- aasemble/django/apps/main/context_processors.py
+++ aasemble/django/apps/main/context_processors.py
@@ -2,10 +2,5 @@ from test_project import settings
 
 
 def internal_name_change(request):
-    try:
-        title = settings.AASEMBLE_OVERRIDE_NAME
-    except AttributeError:
-        title = 'aaSemble'
-    # title = getattr(settings, 'AASEMBLE_OVERRIDE_NAME', default='aaSemble')  # This gives TypeError:
-    # getattr() takes no keyword arguments
+    title = getattr(settings, 'AASEMBLE_OVERRIDE_NAME', 'aaSemble')
     return {'title': title}
