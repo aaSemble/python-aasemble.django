fc3dfebe154d9e7e2269724430968d431f87772e
Mayank Kapoor <mayankkapoor@gmail.com>
2015-12-01 23:54:24 +0530

    exclude newer snapshots from v1 queryset
    Now v1 ViewSet's get_queryset filters out all snapshots that have
    visible_in_v1_api = False.

--- aasemble/django/apps/api/v2/views.py
+++ aasemble/django/apps/api/v2/views.py
@@ -6,8 +6,11 @@ from django.conf import settings
 from rest_auth.registration.views import SocialLoginView
 
 from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 
 from aasemble.django.apps.api.v1.views import aaSembleV1Views
+from aasemble.django.apps.mirrorsvc import models as mirrorsvc_models
 
 from . import serializers as serializers_
 
@@ -29,3 +32,32 @@ class aaSembleV2Views(aaSembleV1Views):
         BuildViewSet.filter_backends = (filters.OrderingFilter,)
         BuildViewSet.ordering = ('build_started',)
         return BuildViewSet
+
+    def SnapshotViewSetFactory(selff):
+        class SnapshotViewSet(viewsets.ModelViewSet):
+            lookup_field = selff.default_lookup_field
+            lookup_value_regex = selff.default_lookup_value_regex
+            """
+            API endpoint that allows mirrors to be viewed or edited.
+            """
+            queryset = mirrorsvc_models.Snapshot.objects.all()
+            serializer_class = selff.serializers.SnapshotSerializer
+
+            def get_queryset(self):
+                if self.request.user.is_superuser:
+                    qs = self.queryset.all()
+                else:
+                    qs = self.queryset.filter(mirrorset__owner_id=self.request.user.id)
+
+                tag = self.request.query_params.get('tag', None)
+                if tag is not None:
+                    qs.filter(tags__tag=tag)
+
+                return qs
+
+            def perform_update(self, serializer):
+                if 'mirrorset' in self.request.data or 'timestamp' in self.request.data:
+                    raise ValidationError({'detail': 'Method "PATCH" not allowed.'})
+                serializer.save(owner=self.request.user)
+
+        return SnapshotViewSet
