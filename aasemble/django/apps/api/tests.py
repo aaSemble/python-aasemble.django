3a154c418c25b2f93378a25759342de46e965316
piyush110786 <piyush.pathak@ril.com>
2015-12-02 12:00:48 +0530

    Test case added for snapshot
    Few more test case added in snapshot

--- aasemble/django/apps/api/tests.py
+++ aasemble/django/apps/api/tests.py
@@ -872,6 +872,16 @@ class APIv1Tests(APITestCase):
         self.assertEquals(response.status_code, 400)
         self.assertEquals(response.data, {'mirrorset': ['Invalid hyperlink - No URL match.']})
 
+    def test_create_snapshot_deactivated_user(self, user='frank'):
+        data = {}
+        authenticate(self.client, user)
+        response = self.client.post(self.snapshot_list_url, data, format='json')
+        self.assertEquals(response.status_code, 401)
+        self.assertEquals(response.data, {'detail': 'User inactive or deleted.'})
+
+    def test_create_snapshot_deactivated_super_user(self):
+        self.test_create_snapshot_deactivated_user(user='harold')
+
     def test_patch_snapshot_not_allowed(self):
         snapshot = self.test_create_snapshot()
         # no new mirror set is created because test case intend is different
@@ -887,8 +897,9 @@ class APIv1Tests(APITestCase):
         self.assertEquals(response.status_code, 401)
         self.assertEquals(response.data, {'detail': 'Invalid token header. No credentials provided.'})
 
-    def test_delete_snapshot(self):
+    def test_delete_snapshot(self, user='eric'):
         snapshot = self.test_create_snapshot()
+        authenticate(self.client, user)
         response = self.client.delete(snapshot['self'])
         self.assertEquals(response.status_code, 204)
 
@@ -906,10 +917,7 @@ class APIv1Tests(APITestCase):
         self.assertEquals(response.status_code, 404)
 
     def test_delete_snapshot_super_user(self):
-        snapshot = self.test_create_snapshot()
-        authenticate(self.client, 'george')
-        response = self.client.delete(snapshot['self'])
-        self.assertEquals(response.status_code, 204)
+        self.test_delete_snapshot(user='george')
 
     def test_delete_snapshot_other_user_same_group(self):
         snapshot = self.test_create_snapshot(user='brandon')
@@ -917,17 +925,14 @@ class APIv1Tests(APITestCase):
         response = self.client.delete(snapshot['self'])
         self.assertEquals(response.status_code, 404)
 
-    def test_delete_snapshot_deactivated_super_user(self):
+    def test_delete_snapshot_deactivated_super_user(self, user='harold'):
         snapshot = self.test_create_snapshot()
-        authenticate(self.client, 'harold')
+        authenticate(self.client, user)
         response = self.client.delete(snapshot['self'])
         self.assertEquals(response.status_code, 401)
 
     def test_delete_snapshot_deactivated_other_user(self):
-        snapshot = self.test_create_snapshot()
-        authenticate(self.client, 'frank')
-        response = self.client.delete(snapshot['self'])
-        self.assertEquals(response.status_code, 401)
+        self.test_delete_snapshot_deactivated_super_user('frank')
 
     ##############
     # Auth tests #
