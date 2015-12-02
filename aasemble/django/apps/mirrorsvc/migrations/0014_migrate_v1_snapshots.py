3bf550e5baa1e040afc8d03b61ad20bf6e12337e
Mayank Kapoor <mayankkapoor@gmail.com>
2015-12-02 10:05:31 +0530

    Break up into two migrations
    visible_to_v1_api is set to True initially, then changed to False. All
    old snapshots will have the field as True.

--- aasemble/django/apps/mirrorsvc/migrations/0014_migrate_v1_snapshots.py
+++ aasemble/django/apps/mirrorsvc/migrations/0014_migrate_v1_snapshots.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os
-
-from django.conf import settings
 from django.db import migrations, models
-
-
-def forwards_migrate_snapshots(apps, schema_editor):
-    # Using historical version of Snapshot model
-    Snapshot = apps.get_model("mirrorsvc", "Snapshot")
-    for snapshot in Snapshot.objects.all():
-        # Rename old directory to new directory with uuid, create symlink for old snapshots
-        src_dir = os.path.join(settings.MIRRORSVC_BASE_PATH, 'snapshots', str(snapshot.id))
-        new_dir = os.path.join(settings.MIRRORSVC_BASE_PATH, 'snapshots', str(snapshot.uuid))
-        if os.path.isdir(src_dir) and not os.path.isdir(new_dir):
-            os.rename(src_dir, new_dir)
-            snapshot.visible_to_v1_api = True
-            os.symlink(new_dir, src_dir)
 
 
 class Migration(migrations.Migration):
@@ -30,7 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='snapshot',
             name='visible_to_v1_api',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=True),
         ),
-        migrations.RunPython(forwards_migrate_snapshots),
     ]
