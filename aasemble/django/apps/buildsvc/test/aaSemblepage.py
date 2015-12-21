e1d95cd1ff1073759be78e4111a9d127c580e73e
Jaspreet Walia <walia.jaspreet@gmail.com>
2015-12-22 02:44:11 +0530

    Added delete test for Mirror. Removed redundant create_mirror code from test cases.

--- aasemble/django/apps/buildsvc/test/aaSemblepage.py
+++ aasemble/django/apps/buildsvc/test/aaSemblepage.py
@@ -36,6 +36,26 @@ class BasePage(object):
                 Exceptions.ElementNotVisibleException):
             return False
 
+    def _is_value_displayed(self, locator, value):
+        try:
+            webelement = self.driver.find_element(*locator)
+            element_attribute_value = webelement.get_attribute('value')
+            return element_attribute_value == value
+        except Exception:
+            return False
+
+    '''This method extracts web table row based on input text
+       data to compare and verify as per test case'''
+    def fetch_table_row_details(self, findtext, table_locator):
+        # get all of the rows in the table
+        table_webelement = self.driver.find_element(*table_locator)
+        rows = table_webelement.find_elements(by.By.TAG_NAME, "tr")
+        for row in rows:
+            # Get the columns
+            col = row.find_elements(by.By.TAG_NAME, "td")
+            for s in col:
+                if s.text == findtext:
+                    return row
 
 class SourcePage(BasePage):
     '''This class is to perform all operations on sourcePackage
@@ -171,6 +191,7 @@ class BuildPage(BasePage):
 
 
 class MirrorsPage(BasePage):
+    _table_id_locator = (by.By.CSS_SELECTOR, '.table.table-striped')
 
     @property
     def mirror_button(self):
@@ -205,10 +226,20 @@ class MirrorsPage(BasePage):
     def verify_mirror_visible_by_url(self, value):
         locator = (by.By.LINK_TEXT, value)
         return self._is_element_visible(locator)
+   
+    def verify_mirror_value_visible(self,value):
+        locator = (by.By.NAME,"url")
+        return self._is_value_displayed(locator,value)
 
     def verify_mirror_private(self):
         locator = (by.By.XPATH, ".//table/tbody/tr[1]/td[5][contains(text(), False)]")
         return self._is_element_visible(locator)
+
+    def click_on_mirror_uuid(self,url_id):
+        row = self.fetch_table_row_details(url_id, self._table_id_locator)
+        columns = row.find_elements(by.By.TAG_NAME, "td")
+        # first columns corresponds to uuid
+        (columns[0]).click()
 
 
 class MirrorSetPage(BasePage):
