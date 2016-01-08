4a769e0dc7f7e128cf29fd0a1fc1f5f793452c51
Peter Jensen <pljensen12345@gmail.com>
2016-01-08 11:16:06 +0000

    Deploy to new infra

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@ fi
 
 openssl aes-256-cbc -K $encrypted_8f2ce7add2e8_key -iv $encrypted_8f2ce7add2e8_iv -in deploy.key.enc -out deploy.key -d || echo it failed
 chmod 600 deploy.key
-ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@overcastcloud.com 'cd www; ./deploy.sh ; cd ../dev/current ; ./deploy.sh'
+ssh -i deploy.key -o UserKnownHostsFile=.known_hosts soren@104.197.86.90 'cd srv; cd aasemble/code ; ./deploy.sh ; cd - ; cd overcast/code ; ./deploy.sh'
