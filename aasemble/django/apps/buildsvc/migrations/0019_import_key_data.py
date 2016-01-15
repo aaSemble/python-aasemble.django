64b7449c5b86140ab9423a06d95e6236bb049e13
Peter Jensen <pljensen12345@gmail.com>
2016-01-15 08:17:30 +0000

    Model instance methods are not available during migration
    Copy the code from the models.

--- aasemble/django/apps/buildsvc/migrations/0019_import_key_data.py
+++ aasemble/django/apps/buildsvc/migrations/0019_import_key_data.py
@@ -4,12 +4,19 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+from aasemble.django.utils import run_cmd
+
 
 def import_key_data(apps, schema_editor):
     Repository = apps.get_model("buildsvc", "Repository")
     for repo in Repository.objects.all():
-        repo.key_data = repo._key_data()
+        repo.key_data = key_data(repo)
         repo.save()
+
+
+def key_data(repository):
+    if repository.key_id:
+        return run_cmd(['gpg', '-a', '--export', repository.key_id])
 
 
 class Migration(migrations.Migration):
