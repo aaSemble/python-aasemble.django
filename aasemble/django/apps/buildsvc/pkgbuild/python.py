d9ce0c713787c7219d9dde959447028f6680b8f6
Anshu Prateek <anshu.prateek@ril.com>
2016-01-27 21:18:36 +0530

    Fixing tox/travis errors

--- aasemble/django/apps/buildsvc/pkgbuild/python.py
+++ aasemble/django/apps/buildsvc/pkgbuild/python.py
@@ -1,10 +1,9 @@
 import os.path
+import yaml
 
 from aasemble.django.apps.buildsvc.pkgbuild import PackageBuilder, PackageBuilderRegistry
 from aasemble.utils import run_cmd
 from django.template.loader import render_to_string
-
-import yaml
 
 
 class PythonBuilder(PackageBuilder):
