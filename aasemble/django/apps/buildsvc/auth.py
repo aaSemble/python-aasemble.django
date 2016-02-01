d2e8e10a8e7a1debb0e78df0981cedd38bcde8c8
Peter Jensen <pljensen12345@gmail.com>
2016-02-01 14:15:00 +0000

    Make auth code more readable

--- aasemble/django/apps/buildsvc/auth.py
+++ aasemble/django/apps/buildsvc/auth.py
@@ -1,25 +1,54 @@
+from django.contrib.auth.models import User
+
 class BuildSvcAuthzBackend(object):
+    known_actions = ('add', 'delete', 'change')
+
+    known_app_models = (('buildsvc', 'repository'),
+                        ('buildsvc', 'packagesource'),
+                        ('buildsvc', 'externaldependency'),
+                        ('mirrorsvc', 'mirror'),
+                        ('mirrorsvc', 'mirrorset'),
+                        ('mirrorsvc', 'snapshot'))
+
+    def get_user(self, user_id):
+        """The Django documentation says this method is required[1],
+        but it's not clear when it'll ever get called. It's very
+        straightforward, though, so here it is."""
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
     def authenticate(self, *args, **kwargs):
+        """Seeing as this backend is strictly for authorization, not
+        authentication, just return None. Django will keep trying
+        backends until one agrees to authenticate the user"""
         return None
+
+    def get_app_perm_action_model(self, perm):
+        app, perm = perm.split('.')
+        action, model = perm.split('_')
+        return app, perm, action, model
 
     def has_perm(self, user_obj, perm, obj=None):
         if not user_obj.is_active:
             return False
 
-        app, perm_ = perm.split('.')
-        action, model = perm_.split('_')
+        app, perm, action, model = self.get_app_perm_action_model(perm)
 
-        if action not in ('add', 'delete', 'change'):
+        if action not in self.known_actions:
             return False
 
-        if ((((app == 'buildsvc') and (model in ('repository', 'packagesource', 'externaldependency'))) or
-             ((app == 'mirrorsvc') and (model in ('mirror', 'mirrorset', 'snapshot'))))):
-            # This is pretty nuts. If we don't return True here,
-            # django-rest-framework aborts immediately. So, we say
-            # "sure!" and hope it asks for permission for the specific
-            # object.
+        if (app, model) in self.known_app_models:
+            # Django will first ask if the user is authorized to perform
+            # the given action on the given model *at all*, and subsequently
+            # check for access to a specific instance of the given model.
+            # Hence, we have to return True here and just hope that it's
+            # followed up by an object-level check.
             if obj is None:
                 return True
 
             if hasattr(obj, 'user_can_modify'):
                 return obj.user_can_modify(user_obj)
+            else:
+               return False
